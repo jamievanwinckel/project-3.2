d5f8ef29e208ca8d63f83b131ab238b44f9dcc98
Jamie Van Winckel <jvanwinckel@mail.csuchico.edu>
2017-01-04 17:23:14 -0800

    adding the second part of the project online

---
+++ cards.js
@@ -0,0 +1,313 @@
+//Created by Jamie Van Winckel
+var _2h = {
+    name: "2 Hearts",
+    minValue: 2,
+    maxValue: 2,
+    img: "./card images/2hearts.png"
+};
+var _2s = {
+    name: "2 Spades",
+    minValue: 2,
+    maxValue: 2,
+    img: "./card images/2spades.png"
+};
+var _2d = {
+    name: "2 Diamonds",
+    minValue: 2,
+    maxValue: 2,
+    img: "./card images/2diamonds.png"
+};
+var _2c = {
+    name: "2 Clubs",
+    minValue: 2,
+    maxValue: 2,
+    img: "./card images/2clubs.png"
+};
+var _3h = {
+    name: "3 Hearts",
+    minValue: 3,
+    maxValue: 3,
+    img: "./card images/3hearts.png"
+};
+var _3s = {
+    name: "3 Spades",
+    minValue: 3,
+    maxValue: 3,
+    img: "./card images/3spades.png"
+};
+var _3d = {
+    name: "3 Diamonds",
+    minValue: 3,
+    maxValue: 3,
+    img: "./card images/3diamonds.png"
+};
+var _3c = {
+    name: "3 Clubs",
+    minValue: 3,
+    maxValue: 3,
+    img: "./card images/3clubs"
+};
+var _4h = {
+    name: "4 Hearts",
+    minValue: 4,
+    maxValue: 4,
+    img: "./card images/4hearts.png"
+};
+var _4s = {
+    name: "4 Spades",
+    minValue: 4,
+    maxValue: 4,
+    img: "./card images/4spades.png"
+};
+var _4d = {
+    name: "4 Diamonds",
+    minValue: 4,
+    maxValue: 4,
+    img: "./card images/4diamonds.png"
+};
+var _4c = {
+    name: "4 Clubs",
+    minValue: 4,
+    maxValue: 4,
+    img: "./card images/4clubs.png"
+};
+var _5h = {
+    name: "5 Hearts",
+    minValue: 5,
+    maxValue: 5,
+    img: "./card images/5hearts.png"
+};
+var _5s = {
+    name:"5 Spades",
+    minValue: 5,
+    maxValue: 5,
+    img: "./card images/5spades.png"
+};
+var _5d = {
+    name: "5 Diamonds",
+    minValue: 5,
+    maxValue: 5,
+    img: "./card images/5diamonds.png"
+};
+var _5c = {
+    name: "5 Clubs",
+    minValue: 5,
+    maxValue: 5,
+    img: "./card images/5clubs.png"
+};
+var _6h = {
+    name: "6 Hearts",
+    minValue: 6,
+    maxValue: 6,
+    img: "./card images/6hearts.png"
+};
+var _6s = {
+    name: "6 Spades",
+    minValue: 6,
+    maxValue: 6,
+    img: "./card images/6spades.png"
+};
+var _6d = {
+    name: "6 Diamonds",
+    minValue: 6,
+    maxValue: 6,
+    img: "./card images/6diamonds.png"
+};
+var _6c = {
+    name: "6 Clubs",
+    minValue: 6,
+    maxValue: 6,
+    img: "./card images/6clubs.png"
+};
+var _7h = {
+    name: "7 Hearts",
+    minValue: 7,
+    maxValue: 7,
+    img: "./card images/7hearts.png"
+};
+var _7s = {
+    name: "7 Spades",
+    minValue: 7,
+    maxValue: 7,
+    img: ""
+};
+var _7d = {
+    name: "7 Diamonds",
+    minValue: 7,
+    maxValue: 7,
+    img: ""
+};
+var _7c = {
+    name: "7 Clubs",
+    minValue: 7,
+    maxValue: 7,
+    img: ""
+};
+var _8h = {
+    name:"8 Hearts",
+    minValue: 8,
+    maxValue: 8,
+    img: ""
+};
+var _8s = {
+    name: "8 Spades",
+    minValue: 8,
+    maxValue: 8,
+    img: ""
+};
+var _8d = {
+    name: "8 Diamonds",
+    minValue: 8,
+    maxValue: 8,
+    img: ""
+};
+var _8c = {
+    name: "8 Clubs",
+    minValue: 8,
+    maxValue: 8,
+    img: ""
+};
+var _9h = {
+    name: "9 Hearts",
+    minValue: 9,
+    maxValue: 9,
+    img: ""
+};
+var _9s = {
+    name: "9 Spades",
+    minValue: 9,
+    maxValue: 9,
+    img: ""
+};
+var _9d = {
+    name: "9 Diamonds",
+    minValue: 9,
+    maxValue: 9,
+    img: ""
+};
+var _9c = {
+    name: "9 Clubs",
+    minValue: 9,
+    maxValue: 9,
+    img: ""
+};
+var _10h = {
+    name: "10 Hearts",
+    minValue: 10,
+    maxValue: 10,
+    img: "./card images/10hearts.png"
+};
+var _10s = {
+    name: "10 Spades",
+    minValue: 10,
+    maxValue: 10,
+    img: "./card images/10spades.png"
+};
+var _10d = {
+    name: "10 Diamonds",
+    minValue: 10,
+    maxValue: 10,
+    img: "./card images/10diamonds.png"
+};
+var _10c = {
+    name: "10 Clubs",
+    minValue: 10,
+    maxValue: 10,
+    img: "./card images/10clubs.png"
+};
+var aceh = {
+    name: "Ace Hearts",
+    minValue: 1,
+    maxValue: 11,
+    img: ""
+};
+var aces = {
+    name: "Ace Spades",
+    minValue: 1,
+    maxValue: 11,
+    img: ""
+};
+var aced = {
+    name: "Ace Diamonds",
+    minValue: 1,
+    maxValue: 11,
+    img: ""
+};
+var acec = {
+    name: "Ace Clubs",
+    minValue: 1,
+    maxValue: 11,
+    img: ""
+};
+var jackh = {
+    name: "Jack Hearts",
+    minValue: 10,
+    maxValue: 10,
+    img: ""
+};
+var jacks = {
+    name: "Jack Spades",
+    minValue: 10,
+    maxValue: 10,
+    img: ""
+};
+var jackd = {
+    name: "Jack Diamonds",
+    minValue: 10,
+    maxValue: 10,
+    img: ""
+};
+var jackc = {
+    name: "Jack Clubs",
+    minValue: 10,
+    maxValue: 10,
+    img: ""
+};
+var queenh = {
+    name: "Queen Hearts",
+    minValue: 10,
+    maxValue: 10,
+    img: ""
+};
+var queens = {
+    name: "Queen Spades",
+    minValue: 10,
+    maxValue: 10,
+    img: ""
+};
+var queend = {
+    name: "Queen Diamonds",
+    minValue: 10,
+    maxValue: 10,
+    img: ""
+};
+var queenc = {
+    name: "Queen Clubs",
+    minValue: 10,
+    maxValue: 10,
+    img: ""
+};
+var kingh = {
+    name: "King Hearts",
+    minValue: 10,
+    maxValue: 10,
+    img: ""
+};
+var kings = {
+    name: "King Spades",
+    minValue: 10,
+    maxValue: 10,
+    img: ""
+};
+var kingd = {
+    name: "King Diamonds",
+    minValue: 10,
+    maxValue: 10,
+    img: ""
+};
+var kingc = {
+    name: "King Clubs",
+    minValue: 10,
+    maxValue: 10,
+    img: ""
+};
---
+++ pageTwo.js
@@ -0,0 +1,156 @@
+//Created by Jamie Van Winckel
+
+var unshuffledDeck = [_2h, _2s, _2d, _2c, _3h, _3s, _3d, _3c, _4h, _4s, _4d, _4c,
+  _5h, _5s, _5d, _5c,_6h, _6s, _6d, _6c, _7h, _7s, _7d, _7c, _8h, _8s, _8d, _8c,
+  _9h, _9s, _9d, _9c, _10h, _10s, _10d, _10c, aceh, aces, aced, acec,jackh, jacks,
+  jackd, jackc, queenh, queens, queend, queenc, kingh, kings, kingd, kingc];
+
+var playerCards = [];
+var dealerCards = [];
+var shuffledDeck = [];
+
+shuffledDeck = shuffleTheDeck(unshuffledDeck);
+var nextCardToPick = 0;
+var nextCardForPlayer = 0;
+var nextCardForDealer = 0;
+
+function shuffleTheDeck(unshuffledDeck) {
+    //courtesy of w3schools.com, from: http://www.w3schools.com/js/js_array_sort.asp
+    var randomizedDeck = unshuffledDeck.sort(function(a, b){return 0.5 - Math.random()});
+    var inputDeck, outputDeck;
+    inputDeck = randomizedDeck;
+    for (i = 0; i < 5; i++) {
+      outputDeck = cutTheDeck(inputDeck);
+      inputDeck = outputDeck;
+    }
+    return outputDeck;
+}
+function cutTheDeck(inputDeck) {
+  var cutDeck = [];
+  //choose random number from 0-51
+  var randomNumber = Math.floor(Math.random() * 51);
+  //take bottom group and assign to beginning of array
+  var bottomOfDeck = inputDeck.slice(randomNumber);
+  //assign top group to the latter part of array
+  var topOfDeck = inputDeck.slice(0,randomNumber);
+  cutDeck = bottomOfDeck.concat(topOfDeck);
+  return cutDeck;
+}
+function nextCard() {
+  var cardToPick = shuffledDeck[nextCardToPick];
+  nextCardToPick++;
+  //make if statement for whencards are out, shuffle a new deck
+  if (shuffledDeck == 51) {
+    shuffleTheDeck();
+  }
+  return cardToPick;
+}
+function firstDeal() {
+  oneCardToPlayer();
+  oneCardToDealer();
+  oneCardToPlayer();
+  oneCardToDealer();
+  displayPlayerCards();
+  displayDealerCards();
+}
+function displayPlayerCards() {
+  for (i = 0; i < playerCards.length; i++) {
+    var divName = "p" + (i + 1);
+    //var imgName = "id" + (i + 1);
+    var divHolder = document.getElementById(divName);
+    document.getElementById(divName).innerHTML = playerCards[i].name;
+    document.getElementById(divName).className="boxy";
+    //document.getElementById(imgName).src = playerCards[i].img;
+  }
+}
+function displayDealerCards() {
+  for (i = 0; i < dealerCards.length; i++) {
+    var divName = "d" + (i + 1);
+    var imgName = "id1";
+    //var imgName = "id" + (i + 1);
+    var divHolder = document.getElementById(divName);
+    //document.getElementById(divName).innerHTML = dealerCards[i].name;
+    document.getElementById(divName).className="boxy";
+    var purple = document.getElementById(imgName);
+    //document.getElementById(imgName).src = "./card images/kingofhearts.jpg";
+    document.getElementById(imgName).src = "./card images/10clubs.png";
+    //document.getElementById(imgName).src = dealerCards[i].img;
+  }
+}
+function sumOfCards(cards) {
+  var sumTotal = 0;
+  for (i = 0; i < cards.length; i++) {
+    sumTotal += cards[i].maxValue;
+    //make sum of min values
+    var sumOfMinValues;
+    //make sum of max values
+    var sumOfMaxValues;
+  }
+  return sumTotal;
+}
+function oneCardToPlayer() {
+  playerCards[nextCardForPlayer] = nextCard();
+  nextCardForPlayer++;
+}
+function oneCardToDealer() {
+  dealerCards[nextCardForDealer] = nextCard();
+  nextCardForDealer++;
+}
+function hitMe() {
+  oneCardToPlayer();
+  displayPlayerCards();
+  if (sumOfCards(playerCards) > 21) {
+    var btn = document.getElementById("hitMe");
+    btn.disabled = true;
+    btn = document.getElementById("noMoreCards");
+    btn.disabled = true;
+    var playerScore = getCookie("playerScore");
+    if (isNaN(playerScore)) {
+      playerScore = 0;
+    }
+    playerScore++;
+    setCookie("playerScore", playerScore, 1);
+    document.getElementById("finalResults").innerHTML = "You Busted " + playerScore + " Times in the Last Day";
+  }
+}
+function noMoreCards() {
+  while (sumOfCards(dealerCards) < 17) {
+    oneCardToDealer();
+    displayDealerCards();
+  }
+  var holdPlayerScore = sumOfCards(playerCards);
+  var holdDealerScore = sumOfCards(dealerCards);
+  if (holdPlayerScore > 21 && holdDealerScore > 21) {
+    bothBust();
+  }
+  else if (holdDealerScore > 21 && holdPlayerScore <= 21) {
+    playerWins();
+  }
+  else if (holdPlayerScore > holdDealerScore && holdPlayerScore < 21) {
+    playerWins();
+  }
+  else if (holdPlayerScore == 21 && holdDealerScore != 21) {
+    playerWins();
+  }
+  else if (holdDealerScore > holdPlayerScore && holdDealerScore < 21) {
+    dealerWins();
+  }
+  else if (holdPlayerScore == holdDealerScore) {
+    dealerWins();
+  }
+  else if (holdDealerScore == 21) {
+    dealerWins();
+  }
+}
+function bothBust() {
+  document.getElementById("finalResults").innerHTML = "You Both Busted!";
+}
+function playerWins() {
+  document.getElementById("finalResults").innerHTML = "You Won!";
+}
+function dealerWins() {
+  document.getElementById("finalResults").innerHTML = "The Dealer Won!";
+}
+function newGame() {
+  document.getElementById("newGame").reset();
+}
